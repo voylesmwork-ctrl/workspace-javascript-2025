24f6857b52eb38e74f7adf8d2586e0ec767a7d3c
yilianz <yilianz@usca.edu>
2025-10-08 11:46:22 -0400

    update 2025

--- js/hello.js
+++ js/hello.js
@@ -5,16 +5,11 @@
 
 // get the date and time 
 var today = new Date();
-var hoursNow = today.getHours();
+
 
 // display the greeting message 
 // modify the code so that your code display "Good afternoon" whereas time is from 12:00pm to 6:00pm
-var greetings = "";
-if (hoursNow <= 12){
-  greetings = "Good Morning";
-}else{
-  greetings = "Good Evening";
-}
+
 
 
 // write to the document 
