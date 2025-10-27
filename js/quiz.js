9d45db8ab852ebde0711bd7b62e7483488487f30
Matthew Voyles <voylesmwork@gmail.com>
2025-10-27 13:17:02 -0400

    update

--- js/quiz.js
+++ js/quiz.js
@@ -5,18 +5,33 @@ document.getElementById('remove').textContent = num2;
 var num3 = Math.floor(Math.random()*(num2-1) + 1);
 document.getElementById('retrieve').textContent = num3;
 
-var result = num1 + "-" + num2 + "+" + num3;
-switch (Math.floor(Math.random()*4) + 1) {
-    case 1:
-        break;
-    case 2:
-        break;
-    case 3:
-        break;
-    case 4:
-        break;
-}
 document.getElementById('ans1').textContent = num1 + "-" + (num2+num3);
 document.getElementById('ans2').textContent = num1 + "-" + (num2-num3);
 document.getElementById('ans3').textContent = num1 + "-" + num2 + "-" + num3;
 document.getElementById('ans4').textContent = num1 + "-" + num2 + "+" + num3;
+
+function changeStyle(event) {
+    event.preventDefault();
+    var classname = this.getAttribute('class');
+    if (classname === "answer selected") {
+        this.setAttribute('class', "answer");
+        this.children[0].chlidren[0].checked = false;
+    } else {
+        this.setAttribute('class', "answer selected");
+        this.children[0].chlidren[0].checked = true;
+    }
+}
+for (var i = 0; i < 4; i++) {
+    document.getElementsByClassName('answer')[i].addEventListener('click', changeStyle, false);
+}
+
+function checkAnswers() {
+    var opt1 = document.getElementById('ans1').checked;
+    var opt2 = document.getElementById('ans2').checked;
+    var opt3 = document.getElementById('ans3').checked;
+    var opt4 = document.getElementById('ans4').checked;
+    if (!opt1 && opt2 && !opt3 && opt4) {
+        alert("Correct!");
+    }
+}
+document.querySelector('input[value="submit"]').addEventListener('click', checkAnswers);
